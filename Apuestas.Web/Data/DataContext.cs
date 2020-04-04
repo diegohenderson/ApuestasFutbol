0964eb5adec726bf79c1cfb062d3695483166165
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-04-04 00:01:15 -0300

    Added TeamsController Added TeamEntities Added the conection to the db

---
+++ Apuestas.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Apuestas.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apuestas.Web.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TeamEntity> Teams { get; set; }
+    }
+}
